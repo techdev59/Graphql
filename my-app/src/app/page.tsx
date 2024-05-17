479456f758d2a29dd3afe7c4570888c2eea6477d
madhuri-dizayn <madhuri@dizayn.io>
2024-05-17 15:17:23 +0530

    fetch data by graph ql

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+//fetch the data by graphql.
+
+const fetch =require("node-fetch");
+var fs =require("fs");
 
 export default function Home() {
   return (
     <div>
       <h2>Hello this a project graphql</h2>
+
+    
     </div>
   );
 }
